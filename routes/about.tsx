e46c9fff56c1c0dc0bc4dabc39bf0ccf4ed0a995
yokoyamamio <91837578+yokoyamamio@users.noreply.github.com>
2025-05-21 13:00:44 +0800

    Update about.tsx

--- routes/about.tsx
+++ routes/about.tsx
@@ -8,11 +8,11 @@ export default function About() {
           <a class="" href="/">
             <img src="/logo.png" class="h-12 w-12 rounded-lg" />
           </a>
-          <h1 class="font-bold text-[#c5005a]">情趣小游戏</h1>
+          <h1 class="font-bold text-[#c5005a]">小游戏</h1>
         </div>
 
         <div class="mb-6 mt-3 text-[#c5005a] bg-white border border-base-300 p-4 rounded-lg shadow-lg">
-          <h2 class="font-bold mb-4">🎉 9.9 元开通终身会员，权益如下：</h2>{" "}
+          <h2 class="font-bold mb-4"></h2>{" "}
           <ul class="mb-5 list-none list-inside text-left text-sm">
           </ul>
           {/* <br class="my-3 bg-[#c4bcc029]" /> */}
@@ -26,30 +26,29 @@ export default function About() {
           <div class="max-w-screen-md text-[#c5005a] mx-auto p-4">
             <div className="bg-white p-4 rounded-md shadow-md">
               <h2 class="text-xl font-bold mb-4 text-gray-800">
-                🎉 尊贵的会员，您的权益如下：
+                
               </h2>
               <ul class="list-disc list-inside text-left">
                 <li className="mb-2">
-                  🔓 <span className="font-semibold">解锁 100+ 姿势图片</span>
+                   <span className="font-semibold"></span>
                 </li>
                 <li className="mb-2">
-                  🎢{" "}
+                  {" "}
                   <span className="font-semibold">
-                    开通所有飞行棋/任务关卡主题
+                   
                   </span>
                 </li>
                 <li className="mb-2">
-                  ✨{" "}
-                  <span className="font-semibold">永久解锁本应用所有功能</span>
+                  {" "}
+                  <span className="font-semibold"></span>
                 </li>
               </ul>
               <p class="mt-4 font-bold text-blue-500">
-                📞 遇到问题？欢迎添加客服微信 oiovdev，我们将竭诚为您服务！
+                
               </p>
             </div>
 
             <div class="text-sm mt-6">
-              提供大家好玩分享空间，共同成长，不露脸不露点，不低俗不越界
             </div>
           </div>
         )} */}
